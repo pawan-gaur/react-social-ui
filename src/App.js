f84846f409a72e49c654142dfd38bf617051b455
Pawan Gaur <pawangaur001@gmail.com>
2020-09-02 14:42:44 +0530

    Commit Code for NavBar

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    //BEM Naming Convention
+    <div className='app'>
+      <Header />
+
+      {/* App Body */}
+
+      {/* SideBar */}
+
+      {/* Feed */}
+
+      {/* Widget */}
+    </div>
+  );
+}
+
+export default App;
